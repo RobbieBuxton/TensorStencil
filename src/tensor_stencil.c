08681cf4313ae13296de3db21f712c46705f364b
Robbie Buxton <robbiesbuxton@gmail.com>
2022-09-21 12:07:00 +0100

    feat: got first version of the test suite running

--- src/tensor_stencil.c
+++ src/tensor_stencil.c
@@ -18,33 +18,62 @@
 
 int main(int argc, char *argv[])
 {
+	int iterations = 1;
+	int size_start = 400;
+	int size_step = 25;
+	int time_steps_start = 65;
+	int time_steps_step = 0;
+	float *times = malloc(sizeof(float) * 2 * iterations);
+	float *norm_error = malloc(sizeof(float) * iterations);
+
+
+	FILE *fp1 = fopen("results.csv", "w");
+	if (fp1 == NULL)
+	{
+		printf("Error while opening the file.\n");
+		return;
+	}
+
+	for (int i = 0; i < iterations; i ++) 
+	{
+		test(times + 2*i, norm_error + i, size_start + size_step * i, time_steps_start + time_steps_step * i);
+		fprintf(fp1, "%d, %f, %f, %f\n",size_start+size_step*i, times[2*i], times[2*i+1], norm_error[i]);
+	}
+
+	fclose(fp1);
+	
+}
+
+int test(float *times, float* norm_error, int data_size, int time_steps)
+{
+
+
 	struct timespec begin;
 
 	clock_gettime(CLOCK_MONOTONIC, &begin);
 
 	double time_spent[3];
 
-	int time_steps = 100; //65 to match devito
 	int dimension = 3;
-	int data_size = 450;
 	// Must be odd (only 3 supported atm)
 	int stencil_size = 3;
 
-	int enable_devito = 1; 
+	int enable_devito = 1;
+
+	printf("\n######################\nNEW TEST\nDim: %d Size %d Time Steps %d\n\n",dimension, data_size, time_steps);
 
 	struct tensor *starting_tensor = init_tensor(dimension, data_size);
 
 	// Initing the starting data to arb thing
 	for (int i = 0; i < pow(data_size, dimension); i++)
 	{
-		starting_tensor->array[i] = 2; //6.5
+		starting_tensor->array[i] = 6.5;
 	}
 
 	struct star_stencil *stencil = init_stencil(dimension, stencil_size);
 
+	// Vars
 
-	//Vars 
-	
 	float nu = .5;
 	float dx = 2. / (data_size - 1);
 	float dy = 2. / (data_size - 1);
@@ -52,33 +81,36 @@ int main(int argc, char *argv[])
 	float sigma = .25;
 	float dt = sigma * dx * dz * dy / nu;
 
-	float h_x = 1./(data_size - 1);
-	float h_y = 1./(data_size - 1);
-	float h_z = 1./(data_size - 1);
+	float h_x = 1. / (data_size - 1);
+	float h_y = 1. / (data_size - 1);
+	float h_z = 1. / (data_size - 1);
 
-	float a = 0.1; 
-	float centre = (dt*(a*(-2.0/pow(h_x,2)-2.0/pow(h_y,2)-2.0/pow(h_z,2))))+1;
+	float a = 0.1;
+	float centre = (dt * (a * (-2.0 / pow(h_x, 2) - 2.0 / pow(h_y, 2) - 2.0 / pow(h_z, 2)))) + 1;
 
-	//X
-	if (dimension >= 1) {
-		stencil->axis[0] = (dt*a)/pow(h_x,2);
-		stencil->axis[1] = centre/dimension;
-		stencil->axis[2] = (dt*a)/pow(h_x,2);
+	// X
+	if (dimension >= 1)
+	{
+		stencil->axis[0] = (dt * a) / pow(h_x, 2);
+		stencil->axis[1] = centre / dimension;
+		stencil->axis[2] = (dt * a) / pow(h_x, 2);
 	}
 
-	//Y
-	if (dimension >= 2) {
-		stencil->axis[3] = (dt*a)/pow(h_y,2);
-		stencil->axis[4] = centre/dimension;
-		stencil->axis[5] = (dt*a)/pow(h_y,2);
+	// Y
+	if (dimension >= 2)
+	{
+		stencil->axis[3] = (dt * a) / pow(h_y, 2);
+		stencil->axis[4] = centre / dimension;
+		stencil->axis[5] = (dt * a) / pow(h_y, 2);
 	}
-	//Z
-	if (dimension >= 3) {
-		stencil->axis[6] = (dt*a)/pow(h_z,2);
-		stencil->axis[7] = centre/dimension;
-		stencil->axis[8] = (dt*a)/pow(h_z,2);
+	// Z
+	if (dimension >= 3)
+	{
+		stencil->axis[6] = (dt * a) / pow(h_z, 2);
+		stencil->axis[7] = centre / dimension;
+		stencil->axis[8] = (dt * a) / pow(h_z, 2);
 	}
-	
+
 	printf("Starting Tensor\n\n");
 	print_tensor(starting_tensor);
 
@@ -88,7 +120,7 @@ int main(int argc, char *argv[])
 
 	time_spent[0] = take_interval(&begin);
 
-	struct tensor *t = multi_basis_contraction(starting_tensor,stencil->out,dimension);
+	struct tensor *t = multi_basis_contraction(starting_tensor, stencil->out, dimension);
 	// printf("Post first basis change\n");
 	// print_tensor(t);
 
@@ -103,40 +135,46 @@ int main(int argc, char *argv[])
 	struct tensor *result = multi_basis_contraction(c, stencil->in, dimension);
 
 	time_spent[3] = take_interval(&begin);
-	
-	//Tensor Stencil
+
+	// Tensor Stencil
 	printf("TensorStencil Result\n\n");
 	print_tensor(result);
 
 	float tensor_stencil_norm = euclidean_norm(result);
 
+	times[0] = time_spent[0] + time_spent[1] + time_spent[2] + time_spent[3];
+
 	printf("###Tensor Stencil Time Spent###\n");
-	printf("Init data        %fs\n",time_spent[0]);
-	printf("1st basis contr: %fs\n",time_spent[1]);
-	printf("Eigen scale:     %fs\n",time_spent[2]);
-	printf("2st basis contr: %fs\n\n",time_spent[3]);
-	printf("Total:           %fs\n\n",time_spent[0]+time_spent[1]+time_spent[2]+time_spent[3]);
-	
-	//Devito only runs in 3d
+	printf("Init data        %fs\n", time_spent[0]);
+	printf("1st basis contr: %fs\n", time_spent[1]);
+	printf("Eigen scale:     %fs\n", time_spent[2]);
+	printf("2st basis contr: %fs\n\n", time_spent[3]);
+	printf("Total:           %fs\n\n", time_spent[0] + time_spent[1] + time_spent[2] + time_spent[3]);
+
+	// Devito only runs in 3d
 	if (dimension == 3 && stencil_size == 3 && enable_devito)
-	{ 
+	{
 		float devito_timer = 0;
-		struct tensor* devito_result = devito_stencil_kernel_adapter(starting_tensor,stencil,time_steps, dt, h_x, h_y, h_z, &devito_timer);
+		struct tensor *devito_result = devito_stencil_kernel_adapter(starting_tensor, stencil, time_steps, dt, h_x, h_y, h_z, &devito_timer);
 
 		printf("Devito Result\n\n");
 		print_tensor(devito_result);
 
 		float devito_stencil_norm = euclidean_norm(devito_result);
 
+		times[1] = devito_timer;
+
 		printf("###Devito Spent###\n\n");
-		printf("Total:           %fs\n\n",devito_timer);
+		printf("Total:           %fs\n\n", devito_timer);
 
 		printf("Euclidean norm\n");
-		printf("Tensor Stencil: %f\n",tensor_stencil_norm);
-		printf("Devito:         %f\n",devito_stencil_norm);
-		printf("Difference:     %f\n",tensor_stencil_norm-devito_stencil_norm);
-		printf("Percent Error:  %f\n\n",(tensor_stencil_norm-devito_stencil_norm)/devito_stencil_norm);
-		
+		printf("Tensor Stencil: %f\n", tensor_stencil_norm);
+		printf("Devito:         %f\n", devito_stencil_norm);
+		printf("Difference:     %f\n", tensor_stencil_norm - devito_stencil_norm);
+		printf("Percent Error:  %f\n\n", (tensor_stencil_norm - devito_stencil_norm) / devito_stencil_norm);
+
+		norm_error[0] = (tensor_stencil_norm - devito_stencil_norm) / devito_stencil_norm;
+
 		destroy_tensor(devito_result);
 	}
 
@@ -145,4 +183,6 @@ int main(int argc, char *argv[])
 	destroy_tensor(c);
 	destroy_tensor(result);
 	destroy_stencil(stencil);
+
+	return 1;
 }
